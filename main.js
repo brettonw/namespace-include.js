192996ef29ab0899277a55d05440d0590df1bf0d
Bretton Wade <brettonw.wade@gmail.com>
2015-12-08 01:16:48 -0500

    move the "include" package into node_modules

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 #! /usr/local/bin/node
 
 // I run this on a mac with Node installed via HomeBrew
-var include = require("./include");
+var include = require("include");
 include ("output.js");
 
-stdout.writeln ("Hello World");
+var main = function () {
+    stdout.writeln ("Hello World");
+}
+
+main ();
