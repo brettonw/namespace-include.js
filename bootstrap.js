ef35fbd6605afdfc589355bf9eec2b3a7b0c9cef
Bretton Wade <brettonw.wade@gmail.com>
2015-12-14 13:18:10 -0500

    bootstrap asks for namespace-include now, but npm makes this untestable.

--- bootstrap.js
+++ bootstrap.js
@@ -1,3 +1,3 @@
-var _namespace = require (".");
+var _namespace = require ("namespace-include");
 var _include = function (name) { _namespace.include (name); }
 var _import = function (name) { _namespace.import (name); }
